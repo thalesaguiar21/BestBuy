0d7edbab71dfa3af08fef3ae28f0353c1986e063
rafaellg79 <rafaellg79@hotmail.com>
2016-08-19 21:22:08 -0300

    Versao inicial do web crawler.
    Depois devemos mudar o nome das classes.

---
+++ src/SpiderLeg.java
@@ -0,0 +1,114 @@
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+public class SpiderLeg {
+    // We'll use a fake USER_AGENT so the web server thinks the robot is a normal web browser.
+    private static final String USER_AGENT =
+            "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/13.0.782.112 Safari/535.1";
+    private List<String> links = new LinkedList<String>();
+    private Document htmlDocument;
+
+
+    /**
+     * This performs all the work. It makes an HTTP request, checks the response, and then gathers
+     * up all the links on the page. Perform a searchForWord after the successful crawl
+     * 
+     * @param url
+     *            - The URL to visit
+     * @return whether or not the crawl was successful
+     */
+    public boolean crawl(String url)
+    {
+        try
+        {
+            Connection connection = Jsoup.connect(url).userAgent(USER_AGENT);
+            Document htmlDocument = connection.get();
+            this.htmlDocument = htmlDocument;
+            if(connection.response().statusCode() == 200) // 200 is the HTTP OK status code
+                                                          // indicating that everything is great.
+            {
+                System.out.println("\n**Visiting** Received web page at " + url);
+            }
+            if(!connection.response().contentType().contains("text/html"))
+            {
+                System.out.println("**Failure** Retrieved something other than HTML");
+                return false;
+            }
+            Elements linksOnPage = htmlDocument.select("a[href]");
+            System.out.println("Found (" + linksOnPage.size() + ") links");
+            for(Element link : linksOnPage)
+            {
+            	String nextUrl = link.absUrl("href");
+            	//por algum motivo o link ta retornando uma string vazia
+            	//procurando somente com prefixo portaltransparencia
+            	//excluindo variação da ordenação
+            	if(nextUrl.equals("") ||
+            	  !(nextUrl.contains("CodOrg=26243") || nextUrl.contains("CodOrgao=26243")) ||
+            	  nextUrl.contains("Ordem=")){
+            		continue;
+            	}
+            	else{
+            		if(nextUrl.matches(".*Pagina=\\d#")){
+            			int pageIndex = nextUrl.lastIndexOf("=");
+            			String prefix = nextUrl.substring(0, pageIndex + 1);
+            			nextUrl = prefix + (Integer.parseInt(nextUrl.substring(pageIndex + 1, nextUrl.length() - 1)) + 1);
+            		}
+            		this.links.add(nextUrl);
+            	}
+            }
+            return true;
+        }
+        catch(IOException ioe)
+        {
+        	System.out.println(ioe);
+            // We were not successful in our HTTP request
+            return false;
+        }
+    }
+
+
+    /**
+     * Performs a search on the body of on the HTML document that is retrieved. This method should
+     * only be called after a successful crawl.
+     * 
+     * @param searchWord
+     *            - The word or string to look for
+     * @return whether or not the word was found
+     */
+    public float searchForWord(String searchWord)
+    {
+        // Defensive coding. This method should only be used after a successful crawl.
+        if(this.htmlDocument.body() == null)
+        {
+            System.out.println("ERROR! Call crawl() before performing analysis on the document");
+            return -1f;
+        }
+        System.out.println("Searching for the word " + searchWord + "...");
+        String bodyText = this.htmlDocument.body().text();
+        if(bodyText.contains(searchWord)){
+        	int i = bodyText.indexOf(searchWord) + searchWord.length();
+        	String str = bodyText.substring(i, i + 15);
+        	str = str.replaceAll("[^\\d,]", "");
+        	str = str.replace(",", ".");
+        	System.out.println("\"" + str + "\"");
+        	return Float.parseFloat(str);
+        }
+        else{
+        	return -1f;
+        }
+    }
+
+
+    public List<String> getLinks()
+    {
+        return this.links;
+    }
+
+}
