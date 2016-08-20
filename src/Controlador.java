1c09db86db34113472837211ac532ebfc54ab008
Thales Aguiar <Thales Aguiar>
2016-08-20 15:46:20 -0300

    SpiderTest removido, Controlador agora possui o método main

--- src/Controlador.java
+++ src/Controlador.java
@@ -1,8 +1,10 @@
 
 public class Controlador {
 
-	public static void main(String[] args) {
-		System.out.println("Hello my friend, stay a while and listen!");
-	}
+	public static void main(String[] args)
+    {
+        Minerador minerador = new Minerador();
+        minerador.search("http://www.portaldatransparencia.gov.br/servidores/OrgaoLotacao-ListaServidores.asp?CodOrg=26243&Pagina=1", "Remuneração básica bruta", new PicaretaDeBFamilia());
+    }
 
 }
