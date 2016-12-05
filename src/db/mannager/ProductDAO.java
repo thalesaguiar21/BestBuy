ae90b715a497bbba0ee73f33f059c3eb3f2c12a2
Thales Aguiar <thalesaguiar21@gmail.com>
2016-12-05 02:31:08 -0300

    Adição de métodos de consulta e remoção genéricos

--- src/db/mannager/ProductDAO.java
+++ src/db/mannager/ProductDAO.java
@@ -1,6 +1,8 @@
 package db.mannager;
 
 import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 
 import framework.DadosGlobais;
 
@@ -10,16 +12,36 @@ public class ProductDAO {
 		String query = "SELECT * FROM Product WHERE idProc = " + id + ";";
 		Map<String, Object> prod = DadosGlobais.getDados().getMyDb().select(query).get(0);
 		
-		Integer mId = (Integer) prod.get("idprod");
-		Integer sId = (Integer) prod.get("seller_idseller");
-		String name = (String) prod.get("name");
-		String cost = (String) prod.get("cost");
-		String tipo = (String) prod.get("tipo");
-		String descr = (String) prod.get("descr");
-		String pusage = (String) prod.get("pusage");
+		Integer mId        = (Integer) prod.get("idprod");
+		Integer sId        = (Integer) prod.get("seller_idseller");
+		String name        = (String) prod.get("name");
+		String cost        = (String) prod.get("cost");
+		String tipo        = (String) prod.get("tipo");
+		String descr       = (String) prod.get("descr");
+		String pusage      = (String) prod.get("pusage");
 		String anounceLink = (String) prod.get("anouncelink");
 		
 		return new Product(mId, sId, name, cost, tipo, descr, pusage, anounceLink);
+	}
+	
+	public List<Product> getByCondition (String condition){
+		String query = "SELECT * FROM Product WHERE " + condition;
+		List<Product> products =  new ArrayList<Product>();
+		List<Map<String, Object>> prod = DadosGlobais.getDados().getMyDb().select(query);
+		
+		for(Map<String, Object> myProd : prod){
+			Integer mId        = (Integer) myProd.get("idprod");
+			Integer sId        = (Integer) myProd.get("seller_idseller");
+			String name        = (String) myProd.get("name");
+			String cost        = (String) myProd.get("cost");
+			String tipo        = (String) myProd.get("tipo");
+			String descr       = (String) myProd.get("descr");
+			String pusage      = (String) myProd.get("pusage");
+			String anounceLink = (String) myProd.get("anouncelink");
+			products.add(new Product(mId, sId, name, cost, tipo, descr, pusage, anounceLink));
+		}
+		
+		return products;
 	}
 	
 	public void insert (Product prod) {
@@ -32,6 +54,11 @@ public class ProductDAO {
 	
 	public void remove (int id) {
 		String rm = "DELETE FROM Product WHERE idProd = " + id + ";";
+		DadosGlobais.getDados().getMyDb().update(rm);
+	}
+	
+	public void removeByCondition(String condition) {
+		String rm = "DELETE FROM Product WHERE " + condition;
 		DadosGlobais.getDados().getMyDb().update(rm);
 	}
 	
--- src/db/mannager/SellerDAO.java
+++ src/db/mannager/SellerDAO.java
@@ -39,6 +39,37 @@ public class SellerDAO {
 		return new Seller(sellId, sName, sAddr, sCpf, sContact, sAnounces);
 	}
 	
+	public List<Seller> getByCondition (String condition) {
+		List<Seller> mySellers = new ArrayList<Seller>();
+		String query = "SELECT * FROM Seller WHERE " + condition;
+		List<Map<String, Object>> sellers = DadosGlobais.getDados().getMyDb().select(query);
+		for (Map<String , Object> seller : sellers) {
+			Integer sellid   = (Integer) seller.get("idseller");
+			String sName    = (String) seller.get("name");
+			String sAddr    = (String) seller.get("addr");
+			String sCpf     = (String) seller.get("cpf");
+			String sContact = (String) seller.get("contact");
+			
+			String queryAnounces = "SELECT * From Product WHERE Seller_idSeller = " + sellid + ";";
+			List<Product> sAnounces = new ArrayList<Product>();
+			List<Map<String, Object>> selAnounces = DadosGlobais.getDados().getMyDb().select(queryAnounces);
+			for (Map<String, Object> prod : selAnounces) {
+				Integer pId         = (Integer) prod.get("idproduct");
+				String pName        = (String) prod.get("name");
+				String pCost        = (String) prod.get("cost");
+				String pTipo        = (String) prod.get("tipo");
+				String pDescr 		= (String) prod.get("descr");
+				String pUsage       = (String) prod.get("pusage");
+				String pAnounceLink = (String) prod.get("anouncelink");
+				sAnounces.add(new Product(pId, sellid, pName, pCost, pTipo, pDescr, pUsage, pAnounceLink));
+			}
+			
+			mySellers.add(new Seller(sellid, sName, sAddr, sCpf, sContact, sAnounces));
+		}
+		
+		return mySellers;
+	}
+	
 	public void insert (Seller seller) {
 		// Insere o Seller no banco
 		String query = "INSERT INTO Seller(name, addr, cpf, contact) "
@@ -57,10 +88,15 @@ public class SellerDAO {
 	}
 	
 	public void remove (Seller seller) {
-		String queryAnounce = "DELETE FROM Product WHERE Seller_idSeller = " + seller.getId() + ";";
+		String queryAnounce = "DELETE FROM Product p, Seller s WHERE p.Seller_idSeller = " 
+							  + seller.getId() + " AND s.idSeller = " + seller.getId() +";";
 		DadosGlobais.getDados().getMyDb().update(queryAnounce);
-		String query = "DELETE FROM Seller WHERE idSeller = " + seller.getId() + " LIMIT 1;";
-		DadosGlobais.getDados().getMyDb().update(query);
+	}
+	
+	public void removebyCondition (String condition) {
+		String queryAnounce = "DELETE FROM Product p, Seller s WHERE " + condition 
+							  + " AND p.Seller_IdSeller = s.idSeller;" ;
+		DadosGlobais.getDados().getMyDb().update(queryAnounce);
 	}
 	
 	public void update (Seller oSeller, Seller nSeller) {
